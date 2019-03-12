705498851c7df0053e20c7ba7173dbbda121ab80
pabushabi <pabushabi@mail.ru>
2019-03-12 22:24:02 +0900

    Added Ui improvements

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,27 @@
 'use strict';
 let fish = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 let _s = $("*");
-if (_s.is('#section') != null) { //adding articles
-    (() => {
+
+if (_s.is('#section') != null) {
+    if (document.documentElement.clientHeight > 760)
+        $('#section').css("margin", "0 34% 0 9.5%");
+    for (let i = 0; i < 20; i++) {
+        let article = $("<article class='art'></article>");
+        let h2 = $('<h2></h2>');
+        let p = $('<p class="text"></p>');
+        let img = $('<img src="/assets/arrow.svg" alt="arrow" class="arrows">');
         if (document.documentElement.clientHeight > 760)
-            $('#section').css("margin", "0 34% 0 9.5%");
-        for (let i = 0; i < 20; i++) {
-            let article = $("<article class='art'></article>");
-            let h2 = $('<h2></h2>');
-            let p = $('<p class="text"></p>');
-            let img = $('<img src="/assets/arrow.svg" alt="arrow" class="arrows">');
-            if (document.documentElement.clientHeight > 760)
-                img.css("top","-120px");
-            article.attr("id", i);
-            h2.attr("id", i);
-            p.attr("id", i);
-            h2.html('Заголовок');
-            $('#section').append(article);
-            article.append(h2);
-            article.append(p);
-            article.append(img);
-            p.html(fish);
-        }
-    })();
+            img.css("top","-120px");
+        article.attr("id", i);
+        h2.attr("id", i);
+        p.attr("id", i);
+        h2.html('Заголовок');
+        $('#section').append(article);
+        article.append(h2);
+        article.append(p);
+        article.append(img);
+        p.html(fish);
+    }
 
     let article = document.getElementsByClassName("art"); //creating animation of opening & closing
     [].forEach.call(article, (el) => {
@@ -72,14 +71,13 @@ if (_s.is('#search_label') !== false) {
             if (document.getElementById("c" + i).checked)
                 chechs[i-1] = i;
 
-        console.log(chechs);
         let req = JSON.stringify({search_req: inp, radio: rad, check: chechs});
         console.log(req);
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "/", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.addEventListener("load", () => {
-            console.log(xhr.response);
+            // console.log(xhr.response);
         });
         xhr.send(req);
     })
@@ -104,9 +102,20 @@ function resume_submit() {
     xhr.open("POST", "/profile",true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(profile);
+    let submit = $("#submit");
+    submit.blur();
+    submit.html("Сохранено!");
+    submit.css({"backgroundColor": "#7FCD51", "color": "rgb(35, 31, 32)"});
+
+    setTimeout(() => {
+        submit.css({"animation-name": "changingColor", "color": "white", "background-color": "crimson"});
+        submit.html("Сохранить");
+    }, 2000)
 }
 
 if (_s.is("#resume-form") !== false) {
+    $(".log").css("display", "none");
+
     if (document.documentElement.clientHeight > 760){
         $(".form-res").eq(0).css("right", "24%");
         $(".prof-inf").eq(0).css("left", "24%");
@@ -119,24 +128,28 @@ if (_s.is("#resume-form") !== false) {
     xhr.addEventListener("load", () => {
         let res = xhr.response;
         let parsed = JSON.parse(res);
-        let {first_name, second_name, birthdate, education, experience, specialization, phone,
-            time_mode, pay_b, pay_t, about} = parsed[0];                    //Это деструктуризация и она крута!
-        console.log(first_name, second_name, birthdate, education, experience, specialization,
-            phone, time_mode, pay_b, pay_t, about);
+        if (parsed.length !== 0) {
+            let {
+                first_name, second_name, birthdate, education, experience, specialization, phone,
+                time_mode, pay_b, pay_t, about
+            } = parsed[0];                    //Это деструктуризация и она крута!
+            console.log(first_name, second_name, birthdate, education, experience, specialization,
+                phone, time_mode, pay_b, pay_t, about);
 
-        $("#i-name").val(first_name);
-        $("#i-secname").val(second_name);
-        $("#i-date").val(birthdate);
-        if (education === 'true')
-            $("#i-ed").prop("checked", 1);
-        else $("#i-ed").prop("checked", 0);
-        $("#i-ex").val(experience);
-        $("#select-spec").val(specialization);
-        $("#i-tel").val(phone);
-        $("#time-mode").val(time_mode);
-        $("#i-payb").val(pay_b);
-        $("#i-payt").val(pay_t);
-        $("#i-about").val(about);
+            $("#i-name").val(first_name);
+            $("#i-secname").val(second_name);
+            $("#i-date").val(birthdate);
+            if (education === 'true')
+                $("#i-ed").prop("checked", 1);
+            else $("#i-ed").prop("checked", 0);
+            $("#i-ex").val(experience);
+            $("#select-spec").val(specialization);
+            $("#i-tel").val(phone);
+            $("#time-mode").val(time_mode);
+            $("#i-payb").val(pay_b);
+            $("#i-payt").val(pay_t);
+            $("#i-about").val(about);
+        }
     });
 
 }
